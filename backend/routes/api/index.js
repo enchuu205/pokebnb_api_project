23c3c256c8b772f09c15b3f6e3c3d3fa7226ed52
Andrew <andrew.ly205@gmail.com>
2024-02-02 06:58:10 -0800

    Updated merge branch of reviews routes and delete images routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,9 @@ const sessionRouter = require('./sessions.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js')
+const spotImagesRouter = require('./spot-images.js')
+const reviewImagesRouter = require('./review-images.js')
+
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
@@ -18,6 +21,10 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter)
 
 router.use('/reviews', reviewsRouter)
+
+router.use('/spot-images', spotImagesRouter)
+
+router.use('/review-images', reviewImagesRouter)
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
