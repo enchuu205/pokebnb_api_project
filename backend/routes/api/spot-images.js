267ad599d2f58f5c92dd62a89beacf78f8e33541
Andrew <andrew.ly205@gmail.com>
2024-02-02 06:37:38 -0800

    Added delete spot image route

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const { Spot, SpotImage, Review, User, ReviewImage } = require('../../db/models');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+// Delete a Spot /api/spot-images/:imageId
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const { user } = req
+    const { imageId } = req.params
+
+    const spotImg = await SpotImage.findOne({ where: { id: imageId } })
+    if (!spotImg) return res.status(404).json({ message: `Spot Image couldn't be found` })
+
+    const spot = await Spot.findOne({ where: { id: spotImg.spotId } })
+    if (spot.ownerId != user.id) return res.status(403).json({ message: "Forbidden" })
+
+    await spotImg.destroy()
+
+    return res.json({ message: "Successfully deleted" })
+})
+
+
+module.exports = router;
